3aa44817d97d6058305c827ef0c1d567792d9e80
RyuJiye <coomicuma@naver.com>
2025-03-14 21:32:13 +0900

    cors edit

--- src/main/java/com/example/jampot/global/config/CorsMvcConfig.java
+++ src/main/java/com/example/jampot/global/config/CorsMvcConfig.java
@@ -10,7 +10,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOriginPatterns("https://localhost:5173", "https://localhost:3000", "https://jampot.co.kr")  // 모든 Origin 허용 (특히 Swagger 테스트용)
+                .allowedOriginPatterns("https://localhost:5173", "https://localhost:3000", "https://jampot.co.kr", "https://jampot.co.kr/swagger-ui", "http://localhost:8080/swagger-ui")  // 모든 Origin 허용 (특히 Swagger 테스트용)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie")
